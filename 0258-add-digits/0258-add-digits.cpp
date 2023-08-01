ac28df2df5e02f129560cfca6c67b44156aa2be7
Imsourabh2001 <96477358+Imsourabh2001@users.noreply.github.com>
2023-08-01 20:02:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (87.58%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        int n=num;
+     while(n>0)
+     {
+         sum=sum+n%10;
+         n=n/10;
+         if(n==0&&sum>9) 
+         {
+             n=sum;
+             sum=0;
+         }
+     }
+       return sum; 
+    }
+};
