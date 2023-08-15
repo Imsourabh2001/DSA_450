6e2af321516312a7592d2b97b8342cd71e1369ab
Imsourabh2001 <96477358+Imsourabh2001@users.noreply.github.com>
2023-08-15 18:16:24 +0530

    Added solution - LeetHub

---
+++ Pattern 19 - GFG/pattern-19.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int star=n;
+        int spc=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=star;j++)cout<<"*";
+            for(int k=0;k<spc;k++)cout<<" ";
+            for(int j=1;j<=star;j++)cout<<"*";
+            cout<<endl;
+            spc=spc+2;
+            star--;
+        }
+        star=1;
+        spc=spc-2;
+         for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=star;j++)cout<<"*";
+            for(int k=0;k<spc;k++)cout<<" ";
+            for(int j=1;j<=star;j++)cout<<"*";
+            cout<<endl;
+            spc=spc-2;
+            star++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
